b1acde605b3741032708790cd430196c1f1783e9
Radnitz Oriasotie <oriasotr@tcd.ie>
2025-08-03 19:17:57 +0100

    Merged testing branch into main

--- app/bash/redeploy-site.sh
+++ app/bash/redeploy-site.sh
@@ -2,7 +2,5 @@
 
 cd /root/portfolio-site
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-systemctl daemon-reload
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
